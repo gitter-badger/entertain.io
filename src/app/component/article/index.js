214a1ceea30294258c15e5034433a70e2cabafb9
michaelzoidl <michael@zoidl.de>
2015-07-24 16:58:28 +0200

    Add article component

---
+++ src/app/component/article/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <article className="component--article">
+        <span>Some article</span>
+      </article>
+    );
+  }
+}
