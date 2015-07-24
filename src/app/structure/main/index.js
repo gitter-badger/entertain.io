f1a1c925a5ef684f152be8c160695049180a0c87
michaelzoidl <michael@zoidl.de>
2015-07-24 20:31:39 +0200

    Update stylings

--- src/app/structure/main/index.js
+++ src/app/structure/main/index.js
@@ -8,14 +8,16 @@ export default class extends Component {
     return (
       <div className="structure--main">
         <div className="header">
+          <Link className="logo" to="/">
+            <span className="entertain-part">ENTERTAIN</span>
+            <span className="io-part">IO</span>
+          </Link>
           <Link to="/">Article-Collection</Link>
         </div>
         <div className="body">
           <RouteHandler/>
         </div>
-        <div>
-
-        </div>
+        <div className="footer"></div>
       </div>
     );
   }
