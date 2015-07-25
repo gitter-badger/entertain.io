d639cf64e166736bd641f170297f6f50ad9e6ce0
Jonathan Häberle <jonathan.haeberle@gmail.com>
2015-07-26 00:37:39 +0200

    Moved components / structures

--- src/app/index.js
+++ src/app/index.js
@@ -5,9 +5,11 @@ import ArticleStore from './store/article-store';
 import ArticleAction from './action/article-action';
 
 import MainStructure from './structure/main';
-import ArticleCollectionStructure from './structure/article-collection';
+import ArticleStructure from './structure/article';
+
+import ArticleCollectionComponent from './component/article-collection';
 import AddArticleComponent from './component/add-article';
-import ArticleComponent from './component/article';
+
 
 import Router from './router/router';
 
@@ -19,8 +21,9 @@ const articleStore = ArticleStore(dispatcher);
 const articleAction = ArticleAction(dispatcher);
 
 const mainStructure = MainStructure();
-const articleComponent = ArticleComponent();
-const articleCollectionStructure = ArticleCollectionStructure(articleStore, articleComponent);
+const articleStructure = ArticleStructure();
+
+const articleCollectionComponent = ArticleCollectionComponent(articleStore, articleStructure);
 const addArticleComponent = AddArticleComponent(articleAction, articleStore);
 
-const router = Router(mainStructure, articleCollectionStructure, addArticleComponent);
+const router = Router(mainStructure, articleCollectionComponent, addArticleComponent);
