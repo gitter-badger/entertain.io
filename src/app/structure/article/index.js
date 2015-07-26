ead062def1584c53e8ba34b3b4e38cf68971c063
michaelzoidl <michael@zoidl.de>
2015-07-26 18:27:56 +0200

    Update classNames in article structure

--- src/app/structure/article/index.js
+++ src/app/structure/article/index.js
@@ -7,7 +7,7 @@ export default function create() {
   class Article extends Component {
     render() {
       return (
-        <article className="component--article">
+        <article className="structure--article">
           <h2>{this.props.title}</h2>
           <p>{this.props.teaser}</p>
           <a href={this.props.url}>{this.props.url}</a>
@@ -18,5 +18,3 @@ export default function create() {
 
   return Article;
 }
-
-
