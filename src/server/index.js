13caed540b9f08a3372eea430a237e9208bc3652
michaelzoidl <michael@zoidl.de>
2015-07-26 18:46:41 +0200

    Use ProdServer Service

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,8 @@
 import DevServer from './services/dev-server';
+import ProdServer from './services/prod-server';
 
 if (process.env.NODE_ENV !== 'production') {
   new DevServer();
 } else {
-  throw new Error('@TODO');
+  new ProdServer();
 }
