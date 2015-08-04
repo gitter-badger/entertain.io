e771b08c67a8c138b6e08942bf31bfe316a1986a
michaelzoidl <michael@zoidl.de>
2015-08-04 09:49:08 +0200

    Style and update article-component

--- src/app/component/article/index.js
+++ src/app/component/article/index.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 require('./style.scss');
 
 export default function create() {
-
   class Article extends Component {
     render() {
       return (
-        <article className="structure--article">
-          <h2>{this.props.title}</h2>
-          <p>{this.props.desc}</p>
-          <a href={this.props.url}>More</a>
+        <article className="article">
+          <div className="meta">
+            <div className="meta-subscription-logo">
+              <a href={''} target="_blank">
+                <img src={''} />
+              </a>
+            </div>
+            <div className="meta-data">
+              <div className="author">
+                <span className="author-by">By</span>
+                <span className="author-name">{''}</span>
+              </div>
+              <div className="publish-date">
+                <span>{moment(this.props.date).format('h:mm:ss a')}</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="media">
+            <a href={this.props.url} target="_blank">
+              { this.props.image ? <img src={this.props.image} /> : '' }
+            </a>
+          </div>
+
+          <div className="article-body">
+            <a className="headline" href={this.props.url} target="_blank">{this.props.title}</a>
+            <p className="description">{this.props.desc}</p>
+          </div>
+
+          <div className="info-bar">
+            <div className="actions">
+              <span className="star">0</span>
+              <span className="comment">0</span>
+            </div>
+            <div className="read-more">
+              <a className="text" href={this.props.url} target="_blank">More</a>
+            </div>
+          </div>
         </article>
       );
     }
