b52123637c519377ccb909464fafdb1cc8c7f8d8
Jonathan Häberle <jonathan.haeberle@gmail.com>
2015-08-05 09:33:23 +0200

    process.env.MONGO_URI

--- src/server/services/storage.js
+++ src/server/services/storage.js
@@ -2,7 +2,7 @@ import MongoQ from '../lib/mongoQ'
 
 export default function create() {
 
-  const MONGO_URI = 'mongodb://127.0.0.1:27017/entertain';
+  const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/entertain';
 
   class Storage {
 
