2b91d39db3c5016d8117ea6c0530c29a9c5b7572
michaelzoidl <michael@zoidl.de>
2015-08-13 13:39:23 +0200

    Add MainAction to handle bodyClicks

---
+++ src/app/action/main-action.js
@@ -0,0 +1,15 @@
+import Debug from 'debug';
+import Dispatcher from '../dispatcher';
+import ConnectionService from '../service/connection';
+const debug = Debug('app:article-action');
+
+class MainAction {
+  bodyClick(e) {
+    Dispatcher.dispatch({
+      eventName: 'body-click',
+      domEvent: e
+    });
+  }
+}
+
+export default new MainAction();
