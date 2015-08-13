625ea6c0bfcfd459ace2406cb448edaea5f72198
michaelzoidl <michael@zoidl.de>
2015-08-13 02:28:03 +0200

    Remove old components and links

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -3,16 +3,14 @@ import Router, {Route} from 'react-router';
 
 import Main from '~/src/app/component/main';
 import ArticleCollection from '~/src/app/component/article-collection';
-import Test from '~/src/app/component/test';
 import AddArticle from '~/src/app/component/add-article';
-import Login from '~/src/app/component/user-management';
+import Login from '~/src/app/component/login';
 
 
 export default class {
   routes = (
     <Route handler={Main}>
       <Route path="/" handler={ArticleCollection}/>
-      <Route path="/test" handler={Test}/>
       <Route path="/login" handler={Login}/>
     </Route>
   );
